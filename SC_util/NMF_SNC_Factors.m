4215109aa5060272305c32bff1013e184feb3355
Selmaan <chettih@fas.harvard.edu>
2017-03-13 16:22:30 -0400

    set threshold for initImages inclusion

--- SC_util/NMF_SNC_Factors.m
+++ SC_util/NMF_SNC_Factors.m
@@ -31,9 +31,7 @@ end
 if ~isempty(initImages)
     useIm = zeros(size(initImages,3),1);
     for nImg = 1:size(initImages,3)
-        tmpImg = medfilt2(initImages(:,:,nImg),[5 5]);
         tmpImg = bwareafilt(tmpImg>0,1);
-        if bwarea(tmpImg) > 30
             useIm(nImg) = 1;
         else
             useIm(nImg) = 0;
