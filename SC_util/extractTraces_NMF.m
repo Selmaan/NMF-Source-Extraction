8b029692ff0641ed9c570a5a115aef6e2f9ab943
Selmaan <chettih@fas.harvard.edu>
2017-03-24 20:10:35 -0400

    update extractTraces_NMF
    Now also deconvolves, part of baseline calculation, using OASIS
    initialized at 1s tau value.
    
    No photobleaching is modeled for baseline dF/F calculation, and traces
    are not corrected for drift. I assume this has been captured previously
    during CNMF.

--- SC_util/extractTraces_NMF.m
+++ SC_util/extractTraces_NMF.m
@@ -1,8 +1,26 @@
-function [dF,A,b,f] = extractTraces_NMF(acqObj)
+function [dF,deconv,denoised,Gs,Lams,A,b,f] = extractTraces_NMF(acqObj,cellLabels,interpFrames)
+
+if nargin < 2 || isempty(cellLabels)
+    cellLabels = cell(length(acqObj.roiInfo.slice),1);
+end
+
+if nargin < 3
+    interpFrames = [];
+end
 
 % Get frame rate and conversion factor for volume imaging
-if acqObj.metaDataSI.SI.hFastZ.enable
-    frame2slice = 1/acqObj.metaDataSI.SI.hFastZ.numFramesPerVolume;
+if isfield(acqObj.metaDataSI,'SI')
+    metaData = acqObj.metaDataSI.SI;
+else
+    metaData = acqObj.metaDataSI;
+end
+
+frameRate = metaData.hRoiManager.scanVolumeRate;
+gDecay = exp(-1/frameRate); %initialize deconv at 1s tau value
+fprintf('Deconvolution initialized at tau-1s, g is %0.3f \n',gDecay);
+
+if metaData.hFastZ.enable
+    frame2slice = 1/metaData.hFastZ.numFramesPerVolume;
 else
     frame2slice = 1/1;
 end
@@ -13,14 +31,25 @@ nBlocks = size(acqObj.syncInfo.sliceFrames,1);
 
 A = cell(1,nSlices);
 b = cell(1,nSlices);
-C = cell(nSlices,nBlocks);
-f = cell(nSlices,nBlocks);
+C = cell(nSlices,1);
+f = cell(nSlices,1);
 
 % Load NMF sources and concatenate data
 for nSlice = 1:nSlices
     D = load(acqObj.roiInfo.slice(nSlice).NMF.filename);
+    load(acqObj.roiInfo.slice(nSlice).NMF.traceFn,'Cf');
     A{nSlice} = D.A;
     b{nSlice} = D.b;
+    nSources = size(A{nSlice},2);
+    nBkgd = size(b{nSlice},2);
+    if ~isempty(cellLabels{nSlice})
+        validSources = cellLabels{nSlice};
+        A{nSlice} = D.A(:,validSources);
+    else
+        validSources = 1:nSources;
+    end
+    thisC = cell(1,nBlocks);
+    thisF = cell(1,nBlocks);
     for nBlock = 1:nBlocks
         % Clip 'extra' frames from slices without complete volume
         if nBlock > 1
@@ -29,39 +58,77 @@ for nSlice = 1:nSlices
             offsetInd = 0;
         end
         blockLength = acqObj.syncInfo.validFrameCount(nBlock) * frame2slice;
-        C{nSlice,nBlock} = D.C(:,offsetInd+1:offsetInd+blockLength);
-        f{nSlice,nBlock} = D.f(:,offsetInd+1:offsetInd+blockLength);
+        thisC{nBlock} = Cf(validSources,offsetInd+1:offsetInd+blockLength);
+        thisF{nBlock} =  Cf(nSources+1:nSources+nBkgd,offsetInd+1:offsetInd+blockLength);
+%         C{nSlice,nBlock} = Cf(1:nSources,offsetInd+1:offsetInd+blockLength);
+%         f{nSlice,nBlock} = Cf(nSources+1:nSources+nBkgd,offsetInd+1:offsetInd+blockLength);
     end
+    C{nSlice} = cell2mat(thisC);
+    f{nSlice} = cell2mat(thisF);
 end
 
+clear Cf D
 %% Get Traces
 dF = cell(nSlices,1);
-f_cat = cell(nSlices,1);
+deconv = cell(nSlices,1);
+denoised = cell(nSlices,1);
+Gs = cell(nSlices,1);
+Lams = cell(nSlices,1);
+
 for nSlice = 1:nSlices
-    % get F_baseline from 1st (tonic) background component
+    % get F_baseline from background component
     thisA = A{nSlice};
     thisB = b{nSlice};
-    thisF = cell2mat(f(nSlice,:));
-    normA = bsxfun(@rdivide,thisA,sum(thisA));
+    thisF = f{nSlice};
+    sumA = full(sum(thisA));
+    normA = bsxfun(@rdivide,thisA,sumA);
     baseF = normA'*thisB * median(thisF,2);
-%     baseF = median(thisF(1,:),2) * normA'*thisB(:,1);
+    thisC = bsxfun(@times,C{nSlice},sumA');
     
-
-    % remove trace baseline for each acquisition block independently
-    for nBlock = 1:nBlocks
-        thisC = C{nSlice,nBlock};
-        for nSig = 1:size(thisC,1)
-            thisC(nSig,:) = removeSourceBaseline(thisC(nSig,:)/baseF(nSig));
+    % Interpolate Data btw Blank Frames
+    if ~isempty(interpFrames)
+        nInterp = length(interpFrames)-1;
+        blankFrames = interpFrames{1};
+        interpVals = 0*thisC(:,blankFrames);
+        interpF = 0*thisF(:,blankFrames);
+        for i=1:nInterp
+            interpVals = interpVals + thisC(:,interpFrames{i+1})/nInterp;
+            interpF = interpF + thisF(:,interpFrames{i+1})/nInterp;
         end
-        C{nSlice,nBlock} = thisC;
+        thisC(:,blankFrames) = interpVals;
+        thisF(:,blankFrames) = interpF;
+        f{nSlice} = thisF;
     end
+
+    % Deconvolve all Signals
+    thisDenoised = nan(size(thisC));
+    thisDeconv = nan(size(thisC));
+    thisB = nan(size(thisC,1),1);
+    thisG = nan(size(thisC,1),1);
+    thisLam = nan(size(thisC,1),1);
+    parfor_progress(size(thisC,1));
+    parfor nSig = 1:size(thisC,1)
+        parfor_progress;
+        [c, s, b, g, lam] = sc_constrained_oasisAR1(double(thisC(nSig,:)), gDecay);
+        thisDenoised(nSig,:) = b+c;
+        thisDeconv(nSig,:) = s;
+        thisB(nSig) = b;
+        thisG(nSig) = g;
+        thisLam(nSig) = lam;
+    end
+    parfor_progress(0);
     
-    thisDF = cell2mat(C(nSlice,:));
+    % Use inferred baseline and background to get dF/F, then scale trace,
+    % denoised and spiking data
+    F_ = double(baseF) + thisB;
+    thisDF = bsxfun(@rdivide,double(thisC),F_);
+    thisDenoised = bsxfun(@rdivide,thisDenoised,F_);
+    thisDeconv = bsxfun(@rdivide,thisDeconv,F_);
     
     % Store data for each slice
     dF{nSlice} = thisDF;
-    f_cat{nSlice} = cell2mat(f(nSlice,:));
+    deconv{nSlice} = thisDeconv;
+    denoised{nSlice} = thisDenoised;
+    Gs{nSlice} = thisG;
+    Lams{nSlice} = thisLam;
 end
-
-% reformat f data to be compatable with dF formatting
-f = f_cat;
