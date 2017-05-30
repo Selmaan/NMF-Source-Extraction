2ab9910716b39108af9e61b183c059befc4d2835
mjlm-office <m.minderer@gmail.com>
2017-05-30 14:04:47 -0400

    Change baseline fitting to 3rd order polynomial

--- SC_util/removeSourceBaseline.m
+++ SC_util/removeSourceBaseline.m
@@ -6,18 +6,22 @@ f(nF+1-rem(nF,binSize):end)=[];
 f = reshape(f, binSize, []);
 baseEstimate = prctile(f, basePrct, 1);
 
-x = 1:nF;
-xBin = (binSize:binSize:length(baseEstimate)*binSize) - binSize/2;
-warnState = warning('off', 'stats:statrobustfit:IterationLimit');
+% 3rd order polynomial fits drift very well and is very robust (in contrast
+% to exp()-based functions).
+x = [(1:nF)', (1:nF)'.^2, (1:nF)'.^3];
+
+xBin = x((binSize:binSize:length(baseEstimate)*binSize) - binSize/2, :);
+warnState = warning('off', 'MATLAB:rankDeficientMatrix');
+
 try
-    b = robustfit(xBin',baseEstimate', 'bisquare', 2);
-catch
+    b = robustfit(xBin, baseEstimate', 'bisquare', 2);
+catch err
     keyboard,
 end
 warning(warnState);
 
 f = fOrig;
-f_ = [ones(nF, 1), x'] * b;
+f_ = [ones(nF, 1), x] * b;
 f_ = f_';
 fSub = f - f_;
 end
